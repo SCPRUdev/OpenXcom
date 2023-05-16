10f82fa4191714c7be69a6515d4fe908c4ee4e55
OsobistSCP <osobist@scpfoundation.net>
2023-05-16 20:37:49 +0700

    Forgot about purchase button here

--- src/Battlescape/CannotReequipState.cpp
+++ src/Battlescape/CannotReequipState.cpp
@@ -95,6 +95,17 @@ CannotReequipState::CannotReequipState(std::vector<ReequipStat> &missingItems, B
 	_lstItems->setSelectable(true);
 	_lstItems->setBackground(_window);
 	_lstItems->setMargin(2);
+
+	if (!_game->getMod()->getNewBaseUnlockResearch().empty())
+	{
+		bool newBasesUnlocked = _game->getSavedGame()->isResearched(_game->getMod()->getNewBaseUnlockResearch(), true);
+		if (!newBasesUnlocked)
+		{
+			_btnManufacture->setVisible(false);
+			_btnPurchase->setVisible(false);
+		}
+	}
+
 }
 
 /**
